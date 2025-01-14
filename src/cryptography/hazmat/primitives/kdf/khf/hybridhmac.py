28548898b44134bc7961e9f8d723879acfd88d5c
JuanMaruriPerez <97404192+JuanMaruriPerez@users.noreply.github.com>
2025-01-14 09:55:05 +0100

    Update comments hybridhmac.py

--- src/cryptography/hazmat/primitives/kdf/khf/hybridhmac.py
+++ src/cryptography/hazmat/primitives/kdf/khf/hybridhmac.py
@@ -12,8 +12,8 @@ class HybridHMAC(KeyDerivationFunction):
         key_material2: bytes,
         length: int,
         salt: Union[bytes, None] = None,
-        operation: str = "concat",
-        secret_key: Union[bytes, None] = None
+        operation: str = "concat",# Concatenacion como default operation
+        secret_key: Union[bytes, None] = None # Clave de HMAC
     ):
         # Verificar que `algorithm` es una instancia de `HashAlgorithm`
         if not isinstance(algorithm, hashes.HashAlgorithm):
@@ -39,12 +39,15 @@ class HybridHMAC(KeyDerivationFunction):
             secret_key = b""
         self._secret_key = secret_key
 
+        # Comprobar operacion soportada
         if self._operation not in ["concat", "xor"]:
             raise ValueError("La operación debe ser 'concat' o 'xor'.")
 
+        # XOR se hace con cadenas de la misma longitud
         if operation == "xor" and len(key_material1) != len(key_material2):
             raise ValueError("Las cadenas key_material1 y key_material2 deben tener la misma longitud para la operación XOR.")
 
+    # Operacion simple de combinacion de cadenas y salt
     def _apply_operation(self) -> bytes:
         if self._operation == "concat":
             # Concatenar key_material1 || salt || key_material2
@@ -55,6 +58,7 @@ class HybridHMAC(KeyDerivationFunction):
             xor_result = bytes(
                 a ^ b for a, b in zip(self._key_material1[:min_length], self._key_material2[:min_length])
             )
+            # Se concatena salt al resultado de la operacion salt
             return xor_result + self._salt
         else:
             raise ValueError("Operación desconocida.")
@@ -70,7 +74,8 @@ class HybridHMAC(KeyDerivationFunction):
 
         # Truncar a la longitud deseada
         return derived_key[:self._length]
-
+        
+    # Verifica operacion criptografica
     def verify(self, expected_key: bytes) -> None:
         derived_key = self.derive()
         if derived_key != expected_key:
